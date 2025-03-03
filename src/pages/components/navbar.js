60fe4f2e2314fc9fe0caa770df67e9a2ffb3b93c
Matthew Loria <loria.matt@gmail.com>
2025-03-02 21:25:38 -0500

    Shrink navbar on smaller screens. Fix deployment scrip for image paths

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -19,9 +19,9 @@ const NavBar = () => {
     return (
       <AppBar position="static" style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}>
         <Container maxWidth="xl">
-          <Toolbar disableGutters>
+          <Toolbar disableGutters variant="dense">
             <StaticImage src='../../images/WMTC_Logo.png' alt='WMTC Logo' style={{ width: '50px', height: '50px' }} />
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end', overflow: 'auto' }}>
                 <Button key='About' sx={{ my: 2, color: 'white', display: 'block' }} onClick={handleAboutClick}>About</Button>
                 <Button key='About' sx={{ my: 2, color: 'white', display: 'block' }}>Meet the Team</Button>
                 <Button key='About' sx={{ my: 2, color: 'white', display: 'block' }}>Schedule</Button>
