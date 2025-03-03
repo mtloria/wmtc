1983229a681cb171632844119a21ab08626a39fa
Matthew Loria <loria.matt@gmail.com>
2025-03-02 22:35:05 -0500

    Stub meet-the-team. If not on home page, then scroll about into view when clicked

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -6,41 +6,48 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { navigate } from "gatsby"
 import { StaticImage } from 'gatsby-plugin-image';
-
-const pages = ['Home', 'About', 'Meet the team', 'Schedule/Results/Other'];
 
 const NavBar = () => {
     const handleAboutClick = () => {
         const aboutSection = document.getElementById('about-section');
         if (aboutSection) {
             aboutSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        } else {
+            navigate('/');
+            setTimeout(() => {
+                const aboutSection = document.getElementById('about-section');
+                if (aboutSection) {
+                    aboutSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                }
+            }, 200);
         }
     };
 
     return (
       <AppBar position="static" style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters variant="dense">
-        <StaticImage src='../../images/WMTC_Logo.png' alt='WMTC Logo' style={{ width: '50px', height: '50px' }} />
-        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end', overflow: 'auto' }}>
-          <Button key='About' sx={{ my: 2, color: 'white', display: 'block' }} onClick={handleAboutClick}>About</Button>
-          <Button key='Meet the Team' sx={{ my: 2, color: 'white', display: 'block' }}>Meet the Team</Button>
-          <Button key='Schedule' sx={{ my: 2, color: 'white', display: 'block' }}>Schedule</Button>
-        </Box>
-        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'flex-end', overflow: 'auto' }}>
-          <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-          >
-          <MenuIcon />
-          </IconButton>
-        </Box>
-        </Toolbar>
-      </Container>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters variant="dense">
+            <StaticImage src='../../images/WMTC_Logo.png' alt='WMTC Logo' style={{ width: '50px', height: '50px' }} />
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end', overflow: 'auto' }}>
+              <Button key='About' sx={{ my: 2, color: 'white', display: 'block' }} onClick={handleAboutClick}>About</Button>
+              <Button key='Meet the Team' sx={{ my: 2, color: 'white', display: 'block' }} onClick={()=>{navigate('/meet-the-team')}}>Meet the Team</Button>
+              <Button key='Schedule' sx={{ my: 2, color: 'white', display: 'block' }}>Schedule</Button>
+            </Box>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, justifyContent: 'flex-end', overflow: 'auto' }}>
+              <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </Container>
       </AppBar>
     );
   }
---
+++ src/pages/meet-the-team.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import NavBar from './components/navbar';
+
+const MeetTheTeamPage = () => {
+    return (
+        <><NavBar /><div>
+            <h1>Meet the Team</h1>
+            <p>Insert team members here.</p>
+        </div></>  
+    )
+}
+
+export default MeetTheTeamPage;
