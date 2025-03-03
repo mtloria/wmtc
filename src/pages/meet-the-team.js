c45a140ee4c6a0e26a29141bd350586efa785475
Matthew Loria <loria.matt@gmail.com>
2025-03-03 14:09:47 -0500

    Fix several quirks when reloading from page other than index

--- src/pages/meet-the-team.js
+++ src/pages/meet-the-team.js
@@ -2,6 +2,13 @@ import * as React from 'react';
 import NavBar from '../components/navbar';
 
 const MeetTheTeamPage = () => {
+    React.useEffect(() => {
+        // Handle page reload quirks
+        document.body.style.margin = '0';
+        document.body.style.padding = '0';
+        document.title = 'Meet the Team | WMTC';
+    }, []);
+
     return (
         <><NavBar /><div>
             <h1>Meet the Team</h1>
