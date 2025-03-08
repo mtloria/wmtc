7fb373e53d437eacfd40a34ee34a7322f8a91457
Matthew Loria <loria.matt@gmail.com>
2025-03-07 21:38:27 -0500

    Customize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,27 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as React from "react";
+import { Link } from "gatsby";
+import { Box, Typography } from "@mui/material";
+import { StaticImage } from 'gatsby-plugin-image';
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <Box sx={{ padding: 4, textAlign: "center" }}>
+      <Typography variant="h1" gutterBottom sx={{ fontWeight: "bold", color: "#202a44" }}>
+        Page Not Found
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 2 }}>
+        <StaticImage src='../images/WMTC_Logo.png' alt="WMTC Logo" style={{ width: "100%", maxWidth: 200, margin: "0 auto", objectFit: "cover", height: "auto" }} />
+      </Box>
+      <Typography variant="body1" sx={{ marginTop: 4 }}>
+        Sorry, we couldn’t find what you were looking for.
+      </Typography>
+      <Typography variant="body1" sx={{ marginTop: 2 }}>
+        <Link to="/" style={{ color: "#1e88e5", textDecoration: "none", fontWeight: "bold" }}>Go back to the homepage</Link>.
+      </Typography>
+    </Box>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>Page Not Found</title>;
