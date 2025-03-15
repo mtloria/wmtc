111801159a8ec6bb5ef5aa83cc9b2f4dfcca9918
Matthew Loria <loria.matt@gmail.com>
2025-03-15 10:03:25 -0400

    Populate elite athlete data

--- src/data/elites.js
+++ src/data/elites.js
@@ -20,6 +20,55 @@ const elites = [
     jobTitle: 'GDP Engineer: Rolls Royce',
     prs: ['5K: 13:48', '10K: 28:42', 'Half Marathon: 1:04:14'],
   },
+  {
+    name: 'Quinn Gallagher',
+    image: 'quinn_gallagher',
+    location: 'Grand Rapids, MI',
+    jobTitle: 'Mechanical Engineer',
+    prs: ['8K (XC): 23:37', '10K (XC): 30:33', 'Mile: 4:10'],
+  },
+  {
+    name: 'Johnnie Guy',
+    image: 'johnnie_guy',
+    location: 'Leopold, IN',
+    jobTitle: 'Mechanical Engineer',
+    prs: ['5K: 13:39', '10K: 28:34', 'Half Marathon: 1:06:47'],
+  },
+  {
+    name: 'Caleb Kerr',
+    image: 'caleb_kerr',
+    location: 'Zionsville, IN',
+    jobTitle: 'Associate Director: Eli Lilly',
+    prs: ['5K: 14:16', '10K: 29:04', 'Half Marathon: 1:04:36', 'Marathon: 2:14:50'],
+  },
+  {
+    name: 'Rob Mullett',
+    image: 'rob_mullett',
+    location: 'Indianapolis, IN',
+    jobTitle: 'Event & Operations Manager: Beyond Monumental',
+    prs: ['Mile: 3:59.37', '5K: 13:41', 'Half Marathon: 1:04:33'],
+  },
+  {
+    name: 'Damon Pruett',
+    image: 'damon_pruett',
+    location: 'Indianapolis, IN',
+    jobTitle: 'Quality Assurance: Continental General',
+    prs: ['5K: 14:33', '10K: 29:52', 'Half Marathon: 1:06:20', 'Marathon: 2:22:48'],
+  },
+  {
+    name: 'Hannah Stoffel',
+    image: 'hannah_stoffel',
+    location: 'Indianapolis, IN',
+    jobTitle: 'Occupational Therapy Doctoral Student',
+    prs: ['1500m: 4:15', 'Half Marathon: 1:15:28', 'Marathon: 2:46:42'],
+  },
+  {
+    name: 'Anthony Williams',
+    image: 'anthony_williams',
+    location: 'Indianapolis, IN',
+    jobTitle: 'Medical Resident: Univ. Utah',
+    prs: ['5K: 14:35', '10K: 30:35', 'Half Marathon: 1:07:29', 'Marathon: 2:19:26'],
+  },
 ];
 
 export default elites;
