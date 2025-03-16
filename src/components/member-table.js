6bb51588b24178bf0153c903336873dbf158c77d
Matthew Loria <loria.matt@gmail.com>
2025-03-15 22:51:19 -0400

    Sort member table alphabetically. Allow column sorting on name column

--- src/components/member-table.js
+++ src/components/member-table.js
@@ -1,16 +1,28 @@
 import * as React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Accordion, AccordionSummary, AccordionDetails, Typography, useMediaQuery, Box } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Accordion, AccordionSummary, AccordionDetails, Typography, useMediaQuery, Box, TableSortLabel } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import members from '../data/members';
+import _ from 'lodash';
+
+members.forEach(member => {
+  member.name = `${member.firstName} ${member.lastName}`;
+});
 
 const MemberTable = () => {
   const isSmallScreen = useMediaQuery('(max-width:600px)');
+  const [order, setOrder] = React.useState('asc');
+
+  const handleSort = () => {
+    setOrder((prevOrder) => (prevOrder === 'asc' ? 'desc' : 'asc'));
+  };
+
+  const sortedMembers = _.orderBy(members, [member => `${member.lastName} ${member.firstName}`.toLowerCase()], [order]);
 
   return (
     <>
       {isSmallScreen ? (
         <Box style={{ maxHeight: '70vh', overflowY: 'auto', border: '1px solid #ccc', borderRadius: '8px', margin: '10px' }}>
-          {members.map((member) => (
+          {sortedMembers.map((member) => (
             <Accordion key={member.id} style={{ margin: '0', border: '1px solid #ddd', borderRadius: '4px' }}>
               <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ backgroundColor: '#f5f5f5' }}>
                 <Typography>{member.name}</Typography>
@@ -29,7 +41,11 @@ const MemberTable = () => {
           <Table stickyHeader>
             <TableHead>
               <TableRow style={{ backgroundColor: '#f5f5f5' }}>
-                <TableCell style={{ fontWeight: 'bold' }}>Name</TableCell>
+                <TableCell style={{ fontWeight: 'bold' }}>
+                  <TableSortLabel active direction={order} onClick={handleSort}>
+                    Name
+                  </TableSortLabel>
+                </TableCell>
                 <TableCell style={{ fontWeight: 'bold' }}>Occupation</TableCell>
                 <TableCell style={{ fontWeight: 'bold' }}>Location</TableCell>
                 <TableCell style={{ fontWeight: 'bold' }}>PR Distance</TableCell>
@@ -37,7 +53,7 @@ const MemberTable = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {members.map((member) => (
+              {sortedMembers.map((member) => (
                 <TableRow key={member.id} hover>
                   <TableCell>{member.name}</TableCell>
                   <TableCell>{member.jobTitle}</TableCell>
--- src/data/members.js
+++ src/data/members.js
@@ -1,202 +1,231 @@
 const members = [
   {
-    name: 'Brady Harless',
+    firstName: 'Brady',
+    lastName: 'Harless',
     jobTitle: 'Cash Management Specialist',
     location: 'Indianapolis, IN',
     PRDistance: '5K',
     PRTime: '14:52'
   },
   {
-    name: 'Charlie Ro',
+    firstName: 'Charlie',
+    lastName: 'Ro',
     jobTitle: 'Manufacturing Engineer',
     location: 'Royal Oak, MI',
     PRDistance: 'Marathon',
     PRTime: '2:31:41'
   },
   {
-    name: 'Emily Wagoner',
+    firstName: 'Emily',
+    lastName: 'Wagoner',
     jobTitle: 'Physical Therapist',
     location: 'South Bend, IN',
     PRDistance: 'Half Marathon',
     PRTime: '1:21:37'
   },
   {
-    name: 'Ethan Halloran',
+    firstName: 'Ethan',
+    lastName: 'Halloran',
     jobTitle: 'Technical Services',
     location: 'Madison, WI',
     PRDistance: '5K',
     PRTime: '17:22'
   },
   {
-    name: 'Jackson Krieg',
+    firstName: 'Jackson',
+    lastName: 'Krieg',
     jobTitle: 'Sales Executive',
     location: 'Fishers, IN',
     PRDistance: '5K',
     PRTime: '14:33'
   },
   {
-    name: 'Jacob Sjogren',
+    firstName: 'Jacob',
+    lastName: 'Sjogren',
     jobTitle: 'Mechanical Engineer',
     location: 'Royal Oak, MI',
     PRDistance: '50-Miler',
     PRTime: '7:27:50'
   },
   {
-    name: 'Jordan Annis',
+    firstName: 'Jordan',
+    lastName: 'Annis',
     jobTitle: 'Police Officer',
     location: 'Mishawaka, IN',
     PRDistance: 'Marathon',
     PRTime: '2:27:39'
   },
   {
-    name: 'Josh Manghelli',
+    firstName: 'Josh',
+    lastName: 'Manghelli',
     jobTitle: 'Surgical Oncologist',
     location: 'Indianapolis, IN',
     PRDistance: 'Ironman',
     PRTime: '10:02:00'
   },
   {
-    name: 'Josh Puccinelli',
+    firstName: 'Josh',
+    lastName: 'Puccinelli',
     jobTitle: 'Assistant Coach: Track/XC',
     location: 'Carmel, IN',
     PRDistance: 'Half Marathon',
     PRTime: '1:16:00'
   },
   {
-    name: 'Katelyn Murphy',
+    firstName: 'Katelyn',
+    lastName: 'Murphy',
     jobTitle: 'Medical Social Worker',
     location: 'Indianapolis, IN',
     PRDistance: 'Half Marathon',
     PRTime: '1:18:47'
   },
   {
-    name: 'Kelsey Macaddino',
+    firstName: 'Kelsey',
+    lastName: 'Macaddino',
     jobTitle: 'Prosthetic & Orthotic Grad Student',
     location: 'Chicago, IL',
     PRDistance: 'Half Marathon',
     PRTime: '1:31:44'
   },
   {
-    name: 'Libby Bowling',
+    firstName: 'Libby',
+    lastName: 'Bowling',
     jobTitle: 'Economic Development Consultant',
     location: 'Indianapolis, IN',
     PRDistance: '5K',
     PRTime: '17:23'
   },
   {
-    name: 'Mallory Evans',
+    firstName: 'Mallory',
+    lastName: 'Evans',
     jobTitle: 'M.D., Internal Medicine & Pediatrics',
     location: 'Ann Arbor, MI',
     PRDistance: 'Marathon',
     PRTime: '3:21:50'
   },
   {
-    name: 'Mariah Jordan',
+    firstName: 'Mariah',
+    lastName: 'Jordan',
     jobTitle: 'Physical Therapist Assistant',
     location: 'Fort Wayne, IN',
     PRDistance: 'Marathon',
     PRTime: '2:47:26'
   },
   {
-    name: 'Matt Bailey',
+    firstName: 'Matt',
+    lastName: 'Bailey',
     jobTitle: 'Account Specialist',
     location: 'Indianapolis, IN',
     PRDistance: 'Marathon',
     PRTime: '2:42'
   },
   {
-    name: 'Matthew Sraders',
+    firstName: 'Matthew',
+    lastName: 'Sraders',
     jobTitle: 'Process Engineer',
     location: 'Indianapolis, IN',
     PRDistance: '5K',
     PRTime: '15:12'
   },
   {
-    name: 'Michael Howell',
+    firstName: 'Michael',
+    lastName: 'Howell',
     jobTitle: 'Industrial Engineer',
     location: 'Indianapolis, IN',
     PRDistance: 'Half Marathon',
     PRTime: '1:10:56'
   },
   {
-    name: 'Nate Stelflug',
+    firstName: 'Nate',
+    lastName: 'Stelflug',
     jobTitle: 'Medical School Student',
     location: 'Indianapolis, IN',
     PRDistance: 'Marathon',
     PRTime: '2:55'
   },
   {
-    name: 'Oliver Rigg',
+    firstName: 'Oliver',
+    lastName: 'Rigg',
     jobTitle: 'Global Logistics',
     location: 'Indianapolis',
     PRDistance: '1500m',
     PRTime: '3:51'
   },
   {
-    name: 'Pat Foster',
+    firstName: 'Pat',
+    lastName: 'Foster',
     jobTitle: 'Technical Solutions Engineer',
     location: 'Madison, WI',
     PRDistance: 'Marathon',
     PRTime: '2:38:23'
   },
   {
-    name: 'Renee Kearns',
+    firstName: 'Renee',
+    lastName: 'Kearns',
     jobTitle: 'Manufacturing Financial Consultant',
     location: 'Denver, CO',
     PRDistance: '5K',
     PRTime: '17:24'
   },
   {
-    name: 'Robbie Hohlman',
+    firstName: 'Robbie',
+    lastName: 'Hohlman',
     jobTitle: 'Informational Scientist',
     location: 'Carmel, IN',
     PRDistance: 'Marathon',
     PRTime: '2:29:55'
   },
   {
-    name: 'Ryan Kearns',
+    firstName: 'Ryan',
+    lastName: 'Kearns',
     jobTitle: 'Senior Systems Engineer',
     location: 'Denver, CO',
     PRDistance: '1500',
     PRTime: '3:57'
   },
   {
-    name: 'Sam Stage',
+    firstName: 'Sam',
+    lastName: 'Stage',
     jobTitle: 'Contract Administration',
     location: 'Carmel, IN',
     PRDistance: '10K',
     PRTime: '31:23'
   },
   {
-    name: 'Stacy Morozov',
+    firstName: 'Stacy',
+    lastName: 'Morozov',
     jobTitle: 'Physician Assistant',
     location: 'Carmel, IN',
     PRDistance: 'Marathon',
     PRTime: '2:52'
   },
   {
-    name: 'Tom Ohlman',
+    firstName: 'Tom',
+    lastName: 'Ohlman',
     jobTitle: 'Patient Care Technician',
     location: 'Indianapolis, IN',
     PRDistance: '10K',
     PRTime: '30:48'
   },
   {
-    name: 'Ty Buckley',
+    firstName: 'Ty',
+    lastName: 'Buckley',
     jobTitle: 'Data Scientist',
     location: 'Royal Oak, MI',
     PRDistance: '3K',
     PRTime: '8:27'
   },
   {
-    name: 'Will Hackler',
+    firstName: 'Will',
+    lastName: 'Hackler',
     jobTitle: 'Commercial Credit Analyst',
     location: 'Indianapolis, IN',
     PRDistance: 'Half Marathon',
     PRTime: '1:13:19'
   },
   {
-    name: 'Zach Duval',
+    firstName: 'Zach',
+    lastName: 'Duval',
     jobTitle: 'Naval Architect',
     location: 'Virginia Beach, VA',
     PRDistance: '5K',
