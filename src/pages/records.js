663f6c499e5bad691a999a66761951452fa1fbd9
Matthew Loria <loria.matt@gmail.com>
2025-03-16 20:29:12 -0400

    Add records data

--- src/pages/records.js
+++ src/pages/records.js
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import NavBar from '../components/navbar';
+import { Box } from '@mui/material';
 
 const RecordsPage = () => {
 
   return (
-    <>
+    <Box sx={{ backgroundColor: 'background.default' }}>
       <NavBar />
-      <h1>Records</h1>
-    </>
+      <Box sx={{ maxWidth: '1200px', margin: '0 auto', textAlign: 'center', flexGrow: 1 }}>
+        <h1 style={{ fontSize: '2.5rem', marginBottom: '20px' }}>Records</h1>
+      </Box>
+    </Box>
   );
 };
 
